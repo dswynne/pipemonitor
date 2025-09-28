c662b47fbd0e508455ea12d5bef06af665423c00
David Wynne <dswynne16@gmail.com>
2025-09-28 18:25:32 -0400

    wip: added a standard message format to the pipe status report, working on consuming this data with a dummy listener

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,11 +1,43 @@
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc};
 use std::thread;
 use std::time::Duration;
-use pipemonitor::pipe::Pipe;
+use pipemonitor::pipe::{Pipe, PipeMessage};
 
 fn main() {
     // Create a shared Pipe wrapped in Arc<Mutex<>>
-    let pipe = Pipe::new_shared(5, String::from("tcp://127.0.0.1:5555"));
+    let pipe = Pipe::new_shared(5, String::from("tcp://127.0.0.1:5555"), String::from("SharedPipe"));
+
+    // ZMQ listener thread: subscribes and deserializes PipeMessage
+    // TODO: Figure out why this doesn't get any data from the producer
+    // TODO: Implement this within the actual pipemonitor
+    let zmq_listener = thread::spawn(|| {
+        let ctx = zmq::Context::new();
+        let sub = ctx.socket(zmq::SUB).expect("Failed to create SUB socket");
+        sub.connect("tcp://127.0.0.1:5555").expect("Failed to connect SUB");
+        sub.set_subscribe(b"").expect("Failed to subscribe");
+        sub.set_rcvtimeo(500).expect("Failed to set receive timeout"); // 500 ms timeout
+        for _ in 0..10 {
+            match sub.recv_bytes(0) {
+                Ok(msg) => {
+                    match serde_json::from_slice::<pipemonitor::pipe::PipeMessage>(&msg) {
+                        Ok(pipe_msg) => println!("ZMQ Listener: received PipeMessage: name={}, size={}, max_capacity={}", pipe_msg.name, pipe_msg.size, pipe_msg.max_capacity),
+                        Err(e) => println!("ZMQ Listener: failed to deserialize: {}", e),
+                    }
+                },
+                Err(e) => {
+                    if e == zmq::Error::EAGAIN {
+                        println!("ZMQ Listener: receive timed out");
+                    } else {
+                        println!("ZMQ Listener: receive error: {}", e);
+                    }
+                }
+            }
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    // Give the listener time to connect before producing
+    thread::sleep(std::time::Duration::from_millis(300));
 
     // Producer thread: pushes numbers 1..=10 into the pipe
     let pipe_producer = Arc::clone(&pipe);
@@ -24,6 +56,7 @@ fn main() {
     });
 
     // Consumer thread: reads the front of the pipe every 150ms
+    // TODO: Add a second pipe here that does something with the data so that we can see two pipes in our demo
     let pipe_consumer = Arc::clone(&pipe);
     let consumer = thread::spawn(move || {
         for _ in 0..10 {
@@ -39,4 +72,5 @@ fn main() {
 
     producer.join().unwrap();
     consumer.join().unwrap();
+    zmq_listener.join().unwrap();
 }
--- src/pipe.rs
+++ src/pipe.rs
@@ -2,34 +2,51 @@ use std::collections::VecDeque;
 use std::ops::{Index, IndexMut};
 use zmq;
 use std::sync::{Arc, Mutex};
+use serde::{Serialize, Deserialize};
 
-// TODO: Add pipe name to initilization
-// TODO: Figure out thread-safety -> https://blog.rust-lang.org/2015/04/10/Fearless-Concurrency/
+
+#[derive(Serialize, Deserialize)]
+pub struct PipeMessage {
+    pub name: String,
+    pub size: usize,
+    pub max_capacity: usize,
+}
+
+impl PipeMessage {
+    pub fn new(name: String, size: usize, max_capacity: usize) -> Self {
+        PipeMessage { name, size, max_capacity }
+    }
+}
+
 pub struct Pipe<T> {
-    deque: VecDeque<T>,
+    // Inputs
     max_capacity: usize,
     address: String,
+    name: String,
+    // Supporting data structures
+    deque: VecDeque<T>,
     socket: zmq::Socket,
 }
 
 impl<T> Pipe<T> {
-    pub fn new(max_capacity: usize, address: String) -> Self {
+    pub fn new(max_capacity: usize, address: String, name: String) -> Self {
         // Connect to ZMQ socket at the address
         let context = zmq::Context::new();
         let socket = context.socket(zmq::PUB).expect("Failed to create socket");
         socket.connect(&address).expect("Failed to connect to address");
 
         Pipe {
-            deque: VecDeque::with_capacity(max_capacity),
             max_capacity,
             address,
+            name,
+            deque: VecDeque::with_capacity(max_capacity),
             socket,
         }
     }
 
-    /// Create a new Pipe wrapped in Arc<Mutex<>> for thread-safe sharing
-    pub fn new_shared(max_capacity: usize, address: String) -> Arc<Mutex<Self>> {
-        Arc::new(Mutex::new(Self::new(max_capacity, address)))
+    // Create a new Pipe wrapped in Arc<Mutex<>> for thread-safe sharing
+    pub fn new_shared(max_capacity: usize, address: String, name: String) -> Arc<Mutex<Self>> {
+        Arc::new(Mutex::new(Self::new(max_capacity, address, name)))
     }
 
     pub fn push_back(&mut self, item: T) -> Option<T> {
@@ -66,11 +83,17 @@ impl<T> Pipe<T> {
 
     fn report_status(&mut self) {
         // TODO: We will also want to optionally set it up to not be done on every change of the pipe
-        // Send current size of deque out zmq socket
-        // TODO: compose this as some standard message format that also has the pipename
-        let size_str = self.deque.len().to_string();
-        // TODO: Handle send errors
-        let _ = self.socket.send(size_str.as_bytes(), 0);
+        // Send current size of deque out zmq socket as JSON
+        let msg = PipeMessage::new(self.name.clone(), self.len(), self.max_capacity);
+        match serde_json::to_string(&msg) {
+            Ok(json) => {
+                let _ = self.socket.send(json.as_bytes(), 0);
+            },
+            Err(e) => {
+                eprintln!("Failed to serialize PipeMessage: {}", e);
+            }
+        }
+
     }
 
 }
@@ -95,13 +118,13 @@ mod tests {
 
     #[test]
     fn test_pipe_new() {
-        let pipe: Pipe<u32> = Pipe::new(10, String::from("tcp://127.0.0.1:5555"));
+        let pipe: Pipe<u32> = Pipe::new(10, String::from("tcp://127.0.0.1:5555"), String::from("TestPipe"));
         assert_eq!(pipe.max_capacity, 10);
     }
 
     #[test]
     fn test_push_back_within_capacity() {
-        let mut pipe: Pipe<u32> = Pipe::new(3, String::from("tcp://127.0.0.1:5555"));
+        let mut pipe: Pipe<u32> = Pipe::new(3, String::from("tcp://127.0.0.1:5555"), String::from("TestPipe"));
         assert_eq!(pipe.push_back(1), None);
         assert_eq!(pipe.push_back(2), None);
         assert_eq!(pipe.push_back(3), None);
@@ -113,7 +136,7 @@ mod tests {
 
     #[test]
     fn test_push_back_over_capacity() {
-        let mut pipe: Pipe<u32> = Pipe::new(2, String::from("tcp://127.0.0.1:5555"));
+        let mut pipe: Pipe<u32> = Pipe::new(2, String::from("tcp://127.0.0.1:5555"), String::from("TestPipe"));
         assert_eq!(pipe.push_back(10), None);
         assert_eq!(pipe.push_back(20), None);
         // Now at capacity, next push should evict 10
@@ -126,14 +149,14 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_index_out_of_bounds() {
-        let mut pipe: Pipe<u32> = Pipe::new(1, String::from("tcp://127.0.0.1:5555"));
+        let mut pipe: Pipe<u32> = Pipe::new(1, String::from("tcp://127.0.0.1:5555"), String::from("TestPipe"));
         pipe.push_back(42);
         let _ = pipe[1]; // Should panic
     }
 
     #[test]
     fn test_pipe_with_strings() {
-        let mut pipe: Pipe<String> = Pipe::new(2, String::from("tcp://127.0.0.1:5555"));
+        let mut pipe: Pipe<String> = Pipe::new(2, String::from("tcp://127.0.0.1:5555"), String::from("TestPipe"));
         assert_eq!(pipe.push_back("foo".to_string()), None);
         assert_eq!(pipe.push_back("bar".to_string()), None);
         assert_eq!(pipe[0], "foo");
@@ -146,7 +169,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_empty_pipe_index_panic() {
-        let pipe: Pipe<u32> = Pipe::new(2, String::from("tcp://127.0.0.1:5555"));
+        let pipe: Pipe<u32> = Pipe::new(2, String::from("tcp://127.0.0.1:5555"), String::from("TestPipe"));
         let _ = pipe[0]; // Should panic
     }
 }
